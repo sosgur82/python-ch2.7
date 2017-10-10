cf1cec0f6187f5b0b3e0ebf8db8c4423ca0555bb
sosgur82 <sosgur82@gamil.com>
2017-10-10 16:08:26 +0900

    Initial commit

---
+++ test-mymath.py
@@ -0,0 +1,8 @@
+import sys
+# pythonpath에 경로를 동적으로 추가 해주는 방법
+# sys.path.append('/bigdata/PycharmProjects/python_modules')
+import mymath
+
+#print(sys.path)
+print(sys.path)
+print(mymath.add(10, 20))
